876f0d68d9f856fd0d7378f4d82eb04108708a01
周武德 <zhouwude@zhouwudedeMacBook-Pro.local>
2017-01-04 11:32:41 +0800

    ss

---
+++ odd-even-CAShaperLayer/odd-even-CAShaperLayer/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  odd-even-CAShaperLayer
+//
+//  Created by 周武德 on 2017/1/4.
+//  Copyright © 2017年 医明康德. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        fun1()
+        fun1("ss")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    //做镂空的图层
+    func fun1()  {
+        view.backgroundColor = UIColor.white
+        let shaperLayer = CAShapeLayer.init()
+        shaperLayer.fillColor = UIColor.black.withAlphaComponent(0.5).cgColor
+        let path1 = CGMutablePath.init()
+        path1.addRect(view.bounds)
+        let path2 = CGMutablePath.init()
+        path2.addRect(CGRect.init(x: 100, y: 100, width: 100, height: 100))
+        path1.addPath(path2)
+        shaperLayer.path = path1
+        shaperLayer.fillRule = kCAFillRuleEvenOdd
+        view.layer.addSublayer(shaperLayer)
+    }
+    func fun1(_ test : String)  {
+        let layer = CAShapeLayer.init()
+        let path = CGMutablePath.init()
+        view.backgroundColor = UIColor.white
+        //跟layer的颜色无关 实心部分才能显示出来 如果透明则 原图层也会被 影藏。
+        path.addEllipse(in: CGRect.init(x: 100, y: 100, width: 100, height: 200))
+        layer.lineWidth = 5
+        layer.strokeColor = UIColor.blue.cgColor
+        layer.fillColor = UIColor.clear.cgColor
+        layer.path = path
+        view.layer.mask = layer
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ odd-even-CAShaperLayer/odd-even-CAShaperLayerTests/odd_even_CAShaperLayerTests.swift
@@ -0,0 +1,36 @@
+//
+//  odd_even_CAShaperLayerTests.swift
+//  odd-even-CAShaperLayerTests
+//
+//  Created by 周武德 on 2017/1/4.
+//  Copyright © 2017年 医明康德. All rights reserved.
+//
+
+import XCTest
+@testable import odd_even_CAShaperLayer
+
+class odd_even_CAShaperLayerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
